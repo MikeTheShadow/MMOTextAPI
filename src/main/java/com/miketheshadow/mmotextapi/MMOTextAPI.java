1da3efe34790f99f664f87d0698e2613ab7eabb4
miketheshadow <miketheshadow1@gmail.com>
2022-02-16 08:45:28 -0700

    Prep release

--- src/main/java/com/miketheshadow/mmotextapi/MMOTextAPI.java
+++ src/main/java/com/miketheshadow/mmotextapi/MMOTextAPI.java
@@ -38,38 +38,38 @@ public final class MMOTextAPI extends JavaPlugin implements Listener {
 
     }
 
-    @EventHandler
-    public void onPlayerJoinEvent(PlayerJoinEvent event) {
-
-        Player player = Bukkit.getPlayer("test");
-
-        if(player != null) player.sendMessage("test");
-
-
-        ItemStack stack =
-                new FoodItemBuilder(
-                        Material.COOKED_BEEF,
-                        "Common Steak",
-                        1, 3, 18,18)
-                        .setDescription("A steak so common you might actually be able to hold it down!")
-                        .addStat(ItemStat.AGILITY, 50)
-                        .addStat(ItemStat.STAMINA, 25).build();
-
-        ItemStack stack2 =
-                new FoodItemBuilder(
-                        Material.PORKCHOP,
-                        "PorkChop Of Legendary Porkness",
-                        7, 3, 30,45)
-                        .setDescription(
-                                "Eating this might turn your bones into liquid energy. Hopefully it's worth it.")
-                        .addStat(ItemStat.STRENGTH,250)
-                        .setGrade(Grade.EMPYREAN)
-                        .addStat(ItemStat.STAMINA, 50).build();
-
-        event.getPlayer().getInventory().addItem(stack);
-        event.getPlayer().getInventory().addItem(stack2);
-
-    }
+//    @EventHandler
+//    public void onPlayerJoinEvent(PlayerJoinEvent event) {
+//
+//        Player player = Bukkit.getPlayer("test");
+//
+//        if(player != null) player.sendMessage("test");
+//
+//
+//        ItemStack stack =
+//                new FoodItemBuilder(
+//                        Material.COOKED_BEEF,
+//                        "Common Steak",
+//                        1, 3, 18,18)
+//                        .setDescription("A steak so common you might actually be able to hold it down!")
+//                        .addStat(ItemStat.AGILITY, 50)
+//                        .addStat(ItemStat.STAMINA, 25).build();
+//
+//        ItemStack stack2 =
+//                new FoodItemBuilder(
+//                        Material.PORKCHOP,
+//                        "PorkChop Of Legendary Porkness",
+//                        7, 3, 30,45)
+//                        .setDescription(
+//                                "Eating this might turn your bones into liquid energy. Hopefully it's worth it.")
+//                        .addStat(ItemStat.STRENGTH,250)
+//                        .setGrade(Grade.EMPYREAN)
+//                        .addStat(ItemStat.STAMINA, 50).build();
+//
+//        event.getPlayer().getInventory().addItem(stack);
+//        event.getPlayer().getInventory().addItem(stack2);
+//
+//    }
 
     public static MMOTextAPI getINSTANCE() {
         return INSTANCE;
