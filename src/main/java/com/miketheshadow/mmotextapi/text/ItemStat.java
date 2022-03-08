09f8dc8495b6ad4a05d6f0823c83c89533a061d2
miketheshadow <miketheshadow1@gmail.com>
2022-03-08 07:42:03 -0700

    Added all the item stats

--- src/main/java/com/miketheshadow/mmotextapi/text/ItemStat.java
+++ src/main/java/com/miketheshadow/mmotextapi/text/ItemStat.java
@@ -4,7 +4,25 @@ public enum ItemStat {
     INTELLIGENCE("Intelligence"),
     STAMINA("Stamina"),
     STRENGTH("Strength"),
-    AGILITY("Agility");
+    AGILITY("Agility"),
+    DAMAGE("Damage"),
+    ATTACK_SPEED("Attack Speed"),
+    CRITICAL_RATE("Critical Rate"),
+    CRITICAL_DAMAGE("Critical Damage"),
+    DAMAGE_REDUCTION("Damage Reduction"),
+    DEFENSE("Defense"),
+    BLOCK_RATE("Block Rate"),
+    PARRY_RATE("Parry Rate"),
+    SHIELD_DEFENSE_PEN_RATE("Shield Defense Penetration Rate"),
+    SHIELD_DEFENSE_PEN("Shield Defense Penetration");
+
+    public static float convertStrengthToDamage(float strength) {
+        return (float) (strength * .2);
+    }
+
+    public static float convertStaminaToHealth(float stamina) {
+        return stamina * 12;
+    }
 
     final String readableName;
 
